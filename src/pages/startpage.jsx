4cb797e59187df4da120d125f6b0565aa57b9e25
Olivia isberg <oliviaisberg@gmail.com>
2022-08-24 15:47:08 +0200

    Fixed startpage

--- src/pages/startpage.jsx
+++ src/pages/startpage.jsx
@@ -1,23 +1,13 @@
-
-import Header from '../components/header';
 import Map from '../components/startpagecomponents/map';
-import Vision from "../components/startpagecomponents/vision";
-import Gallery from "../components/startpagecomponents/gallery";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Gallery from '../components/startpagecomponents/gallery';
 import Vision from '../components/startpagecomponents/vision';
+import Gallery from '../components/startpagecomponents/gallery';
 
 function StartPage() {
   return (
     <div>
-      <Header />
       <Vision />
       <Gallery />
-      <Footer />
       <Map />
-      <Vision />
-      <Gallery />
     </div>
   );
 }
