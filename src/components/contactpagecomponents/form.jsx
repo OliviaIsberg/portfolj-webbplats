4b58fe5a3cea9708a5361b9558fefe4cb277c5ae
Simon Eriksson <44813993+Vacannot@users.noreply.github.com>
2022-08-26 23:10:34 +0200

    sdfgsf

--- src/components/contactpagecomponents/form.jsx
+++ src/components/contactpagecomponents/form.jsx
@@ -3,7 +3,6 @@ import '../../css/form.css';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -19,9 +18,10 @@ function Form() {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
-          email: data.get('email'),
-          password: data.get('password'),
+          email: data.get('firstName'),
+          password: data.get('lastName'),
         });
+        alert(member, data.get('firstName'));
       };
 
     const [member, setMember] = React.useState('');
