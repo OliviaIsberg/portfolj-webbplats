a407e407a2e7ca1e4665a5acf631d0d9c03765e7
Max Andersson <maxbjornkga@gmail.com>
2022-08-29 12:59:43 +0200

    Scroll done

--- src/components/skillpagecomponents/sidemenu.jsx
+++ src/components/skillpagecomponents/sidemenu.jsx
@@ -6,6 +6,7 @@ import Developers from "./Skills/Developers"
 
 export default function SideMenu() {
 
+
     const [isShowDesign, setIsShowDesign] = useState(false)
     const [isShowDeve, setIsShowDeve] = useState(false)
     const [isShowScrum, setIsShowScrum] = useState(false)
@@ -21,6 +22,8 @@ export default function SideMenu() {
         setIsShowScrum(current => !current)
     }
     let menuRef = useRef();
+
+
 
     useEffect(() => {
         let handler = (event) => {
@@ -64,34 +67,35 @@ export default function SideMenu() {
 
 
 
-return (
-    <div className="container" > 
-        <div className="buttons">
 
-            <button onClick={handleClickScrum} className="scrumMaster" >Scrum master</button>
-            <button onClick={handleClickDeveloper} className="Developer" >Developer </button>
-            <button onClick={handleClickDesign} className="Designer" >Designer </button>
+    return (
+        <div className="container" >
+            <div className="buttons">
+
+                <button onClick={handleClickScrum} className="scrumMaster" >Scrum master</button>
+                <button onClick={handleClickDeveloper} className="Developer" >Developer </button>
+                <button onClick={handleClickDesign} className="Designer" >Designer </button>
+            </div>
+
+            {isShowDesign && (
+                <div ref={menuRef}>
+                    <Designers />
+                </div>
+            )}
+            {isShowDeve && (
+                <div ref={menuRef}>
+                    <Developers />
+                </div>
+            )}
+            {isShowScrum && (
+                <div ref={menuRef}>
+                    <ScrumMaster />
+                </div>
+            )}
+
+
+
         </div>
-         
-           { isShowDesign && (
-            <div ref={menuRef}>  
-                <Designers />
-            </div>
-           )}
-             { isShowDeve && (
-            <div ref={menuRef}>  
-                <Developers />
-            </div>
-           )}
-             { isShowScrum && (
-            <div ref={menuRef}>  
-                <ScrumMaster />
-            </div>
-           )}
-        
-
-
-    </div>
-);
+    );
 }
 
