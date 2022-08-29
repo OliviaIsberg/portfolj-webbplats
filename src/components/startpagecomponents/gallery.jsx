218d0fca5b6bf9eb18b84edc934facb5c62dc0ae
saralindqvist <sarathereselindqvist@hotmail.com>
2022-08-29 11:28:19 +0200

    changed gallerydesign

--- src/components/startpagecomponents/gallery.jsx
+++ src/components/startpagecomponents/gallery.jsx
@@ -3,5 +3,41 @@ import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 
 export default function Gallery() {
-  return <div className="galleryContainer"></div>;
+  return (
+    <div className="galleryContainer">
+      <h3>Våra kompetenser på Agili</h3>
+
+      <div className="rollsBoxes">
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "#4E4E4E",
+          }}
+        >
+          <Link to="skills">Designer</Link>
+        </Box>
+
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "#4E4E4E",
+          }}
+        >
+          <Link to="skills">Scrummästare</Link>
+        </Box>
+
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "#4E4E4E",
+          }}
+        >
+          <Link to="skills">Utvecklare</Link>
+        </Box>
+      </div>
+    </div>
+  );
 }
