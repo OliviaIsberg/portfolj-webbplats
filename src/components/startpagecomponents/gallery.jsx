9ee673d57ef8af05257ff8cee905127a293c205c
saralindqvist <sarathereselindqvist@hotmail.com>
2022-08-29 10:01:48 +0200

    changed styling for vision

--- src/components/startpagecomponents/gallery.jsx
+++ src/components/startpagecomponents/gallery.jsx
@@ -1,23 +1,7 @@
-import '../../css/gallery.css';
+import "../../css/gallery.css";
 import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 
 export default function Gallery() {
-    return (
-        <div className="galleryContainer">
-            <h3>Roller</h3>
-
-            <div className="rollsBoxes">
-                <Link to="skills">
-                    <Box component="Button" sx={{ height: 300, marginLeft: 4, backgroundColor: 'lightblue'}}>Scrum master</Box>
-                </Link>
-                <Link to="skills">
-                    <Box component="Button" sx={{ height: 300, marginLeft: 4, backgroundColor: 'pink'}}>Designer</Box>
-                </Link>
-                <Link to="skills">
-                    <Box component="Button" sx={{ height: 300, marginLeft: 4, backgroundColor: 'lightgreen'}}>Utvecklare</Box>
-                </Link>
-            </div>
-        </div>
-    )
+  return <div className="galleryContainer"></div>;
 }
