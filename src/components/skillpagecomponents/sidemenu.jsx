9b9211b01e9a2fc18ec1efe2d93f628733e82f0b
Rosanna Pistone <rosanna@pistone.com>
2022-08-29 14:50:39 +0200

    responsivity added for skills

--- src/components/skillpagecomponents/sidemenu.jsx
+++ src/components/skillpagecomponents/sidemenu.jsx
@@ -1,97 +1,93 @@
-import "../../css/Skills/sideMenu.css"
-import { useState, useEffect, useRef } from "react"
-import ScrumMaster from "./Skills/ScrumMaster"
-import Designers from "./Skills/Designers"
-import Developers from "./Skills/Developers"
+import "../../css/Skills/sideMenu.css";
+import { useState, useEffect, useRef } from "react";
+import ScrumMaster from "./Skills/ScrumMaster";
+import Designers from "./Skills/Designers";
+import Developers from "./Skills/Developers";
 
 export default function SideMenu() {
+  const [isShowDesign, setIsShowDesign] = useState(false);
+  const [isShowDeve, setIsShowDeve] = useState(false);
+  const [isShowScrum, setIsShowScrum] = useState(false);
 
-    const [isShowDesign, setIsShowDesign] = useState(false)
-    const [isShowDeve, setIsShowDeve] = useState(false)
-    const [isShowScrum, setIsShowScrum] = useState(false)
+  const handleClickDeveloper = (event) => {
+    setIsShowDeve((current) => !current);
+  };
+  const handleClickDesign = (event) => {
+    setIsShowDesign((current) => !current);
+  };
+  const handleClickScrum = (event) => {
+    setIsShowScrum((current) => !current);
+  };
+  let menuRef = useRef();
 
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setIsShowDesign(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
 
-    const handleClickDeveloper = event => {
-        setIsShowDeve(current => !current)
-    }
-    const handleClickDesign = event => {
-        setIsShowDesign(current => !current)
-    }
-    const handleClickScrum = event => {
-        setIsShowScrum(current => !current)
-    }
-    let menuRef = useRef();
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-    useEffect(() => {
-        let handler = (event) => {
-            if (!menuRef.current.contains(event.target)) {
-                setIsShowDesign(false)
-            }
-        }
-        document.addEventListener("mousedown", handler);
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setIsShowDeve(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
 
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    });
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-    useEffect(() => {
-        let handler = (event) => {
-            if (!menuRef.current.contains(event.target)) {
-                setIsShowDeve(false)
-            }
-        }
-        document.addEventListener("mousedown", handler);
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setIsShowScrum(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
 
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    });
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-    useEffect(() => {
-        let handler = (event) => {
-            if (!menuRef.current.contains(event.target)) {
-                setIsShowScrum(false)
-            }
-        }
-        document.addEventListener("mousedown", handler);
+  return (
+    <div className="container">
+      <div className="buttons">
+        <button onClick={handleClickDesign} className="Designer">
+          Designer{" "}
+        </button>
+        <button onClick={handleClickScrum} className="scrumMaster">
+          Scrummästare
+        </button>
+        <button onClick={handleClickDeveloper} className="Developer">
+          Utvecklare{" "}
+        </button>
+      </div>
 
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    });
-
-
-
-
-return (
-    <div className="container" > 
-        <div className="buttons">
-
-            <button onClick={handleClickScrum} className="scrumMaster" >Scrum Master</button>
-            <button onClick={handleClickDeveloper} className="Developer" >Developer </button>
-            <button onClick={handleClickDesign} className="Designer" >Designer </button>
+      {isShowDesign && (
+        <div ref={menuRef} className="skill">
+          <Designers />
         </div>
-         
-           { isShowDesign && (
-            <div ref={menuRef}>  
-                <Designers />
-            </div>
-           )}
-             { isShowDeve && (
-            <div ref={menuRef}>  
-                <Developers />
-            </div>
-           )}
-             { isShowScrum && (
-            <div ref={menuRef}>  
-                <ScrumMaster />
-            </div>
-           )}
-        
-
-
+      )}
+      {isShowDeve && (
+        <div ref={menuRef} className="skill">
+          <Developers />
+        </div>
+      )}
+      {isShowScrum && (
+        <div ref={menuRef} className="skill">
+          <ScrumMaster />
+        </div>
+      )}
     </div>
-);
+  );
 }
-
--- src/components/startpagecomponents/vision.jsx
+++ src/components/startpagecomponents/vision.jsx
@@ -6,7 +6,7 @@ export default function Vision() {
       <div className="visionTextContainer">
         <h2>CREATIVE MINDS. CREATIVE WORKS.</h2>
         <h1>We Are Agili.</h1>
-        <p>
+        <p className="visionText">
           Vi på Agili anser att alla har rätt till en schysst digital närvaro.
           Vår vision är att tillgodose alla våra kunders önskemål - nya som
           gamla, stora som små. Till skillnad från liknande företag i branschen
