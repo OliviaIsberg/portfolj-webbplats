6d1412a4755753a3f29f25de86aaf5f6160bb71a
saralindqvist <sarathereselindqvist@hotmail.com>
2022-08-31 13:05:00 +0200

    improved styling

--- src/components/startpagecomponents/gallery.jsx
+++ src/components/startpagecomponents/gallery.jsx
@@ -1,5 +1,5 @@
-import '../../css/gallery.css';
-import { Link } from 'react-router-dom';
+import "../../css/gallery.css";
+import { Link } from "react-router-dom";
 
 export default function Gallery() {
   return (
