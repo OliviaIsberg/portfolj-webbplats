c31ad8b6c75fc3c0271e87c6221863df4e95a253
saralindqvist <sarathereselindqvist@hotmail.com>
2022-08-31 14:21:05 +0200

    added pictures and hovereffect for "gallery"

--- src/components/startpagecomponents/gallery.jsx
+++ src/components/startpagecomponents/gallery.jsx
@@ -1,5 +1,8 @@
 import "../../css/gallery.css";
 import { Link } from "react-router-dom";
+import Utvecklare from "../../Assets/utvecklare.jpeg";
+import Designer from "../../Assets/designer.jpeg";
+import Scrummaster from "../../Assets/scrummaster.png";
 
 export default function Gallery() {
   return (
@@ -8,18 +11,21 @@ export default function Gallery() {
 
       <div className="rollsBoxes">
         <div className="divLink">
+          <img className="utvecklare" src={Designer} alt="horse" />
           <Link className="galleryLink" to="/skills/DesignPage">
             Designer
           </Link>
         </div>
 
         <div className="divLink">
+          <img className="utvecklare" src={Scrummaster} alt="horse" />
           <Link className="galleryLink" to="/skills/ScrumPage">
             Scrummästare
           </Link>
         </div>
 
         <div className="divLink">
+          <img className="utvecklare" src={Utvecklare} alt="horse" />
           <Link className="galleryLink" to="/skills/DeveloperPage">
             Utvecklare
           </Link>
