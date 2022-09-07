d93f7f3c05c37ba470a8732dc0163beb6ed6204f
Olivia isberg <oliviaisberg@gmail.com>
2022-09-07 14:25:59 +0200

    Connected backend and fixed end point apis
    Co-authored-by: Anna Özmehak <A-Ozmehak@users.noreply.github.com>
    Co-authored-by: Simon Eriksson <Vacannot@users.noreply.github.com>
    Co-authored-by: Max Andersson <frontMAX@users.noreply.github.com>
    Co-authored-by: Rosanna Pistone <rosannapistone@users.noreply.github.com>

---
+++ src/api.js
@@ -0,0 +1,45 @@
+export const createUser = (user) =>
+  fetch('http://localhost:5000/api/user/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      password: user.password,
+    }),
+  }).then((response) => response.json());
+
+export const getUser = (id) =>
+  fetch('http://localhost:5000/api/users/' + id).then((response) =>
+    response.json()
+  );
+
+export const login = (email, password) =>
+  fetch('http://localhost:5000/api/login/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+// export const readProduct = (id) =>
+//   fetch('/api/products/' + id).then((response) => response.json());
+
+// export const updateProduct = (product) =>
+//   fetch('/api/products/' + product.id, {
+//     method: 'PUT',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//       name: product.name,
+//       image: product.image,
+//       price: product.price,
+//     }),
+//   });
+
+// export const deleteProduct = (id) =>
+//   fetch('/api/products/' + id, {
+//     method: 'DELETE',
+//   });
--- src/components/loginpagecomponents/loginform.jsx
+++ src/components/loginpagecomponents/loginform.jsx
@@ -3,11 +3,10 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { useFormik } from "formik";
-import * as yup from "yup";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { useState } from 'react';
+import { login } from '../../api';
 
 const validationSchema = yup.object({
   password: yup.string().required('Vänligen skriv ditt lösenord').min(5),
@@ -15,32 +14,20 @@ const validationSchema = yup.object({
 });
 
 function LoginForm() {
-
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [register, setRegister] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [LastName, setLastName] = useState('');
+
+  const loginUser = () =>
+    login(formik.values.email, formik.values.password).then((response) =>
+      console.log(response)
+    );
 
   const handleSend = (event) => {
     event.preventDefault();
-    formik.handleSubmit();
-    handleSubmit();
-    alert("logged in / registered");
+    formik.handleSubmit(event);
+    // handleSubmit();
+    // alert('logged in / registered');
   };
-
-  const changeLogin = () => {
-    setRegister(true)
-  }
-
-  const changeRegister = () => {
-    setRegister(false)
-  }
-
-  const handleSubmit = () => {
-    setEmail('')
-    setPassword('')
-  }
 
   const formik = useFormik({
     initialValues: {
@@ -49,168 +36,70 @@ function LoginForm() {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      // alert(JSON.stringify(values, null, 2));
+      setEmail('');
+      setPassword('');
     },
   });
 
-  if(register){ return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          position: 'absolute',
-          left: "40vw",
-          top: "-20vh",
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          backgroundColor: "black",
-          borderRadius: "1rem",
-          padding: "1rem",
-          '@media (max-width: 700px)' : {
-            position: "relative",
-            left: 0,
-            right: 0,
-          }
-        }}
-      >
-        <Typography
-          component="h1"
-          variant="h5"
-          className="contactMember"
-          sx={{ color: 'white' }}
-        >
-          Registera dig
-        </Typography>
-        <Typography
-          component="p"
-          variant="p"
-          className='contactMember'
-          onClick={() => changeRegister()}
-          sx={{ color: "white", textDecoration: "underline", mt: "-1.5rem", ":hover": {
-            color: "#23BE89", cursor: "pointer",
-          } }}>
-            Har du redan ett konto?
-        </Typography>
-        <Box component="form" onSubmit={handleSend}>
-          <Grid container spacing={2}>
-          <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="LastName"
-                label="Lastname"
-                name="LastName"
-                value={LastName}
-                onChange={(event) => setLastName(event.target.value)}
-                autoComplete="LastName"
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-          <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="firstName"
-                label="Firstname"
-                name="firstName"
-                value={firstName}
-                onChange={(event) => setFirstName(event.target.value)}
-                autoComplete="firstName"
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                value={email}
-                autoComplete="email"
-                inputProps={{maxLength: 20}}
-                onChange={(event) => setEmail(event.target.value)}
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                autoComplete="password"
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
+  return (
+    <Box component="form" onSubmit={formik.handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            required
             fullWidth
-            variant="contained"
-            className="center"
+            id="email"
+            label="Email Address"
+            name="email"
+            value={formik.values.email}
+            autoComplete="email"
+            inputProps={{ maxLength: 50 }}
+            onFocus={formik.handleBlur}
+            onChange={formik.handleChange}
+            InputLabelProps={{ className: 'textfield_label' }}
             sx={{
-              mt: 3,
-              mb: 2,
-              width: '250px',
               color: 'white',
-              backgroundColor: '#23BE99',
-              border: '1px solid #23BE99',
+              backgroundColor: 'black',
+              border: !(formik.touched.email && formik.errors.email)
+                ? '1px solid #23BE99'
+                : '1px solid #FF0000',
+              borderRadius: '20px',
+              outline: 'none',
+
+              input: {
+                color: 'white',
+                '&:-webkit-autofill': {
+                  WebkitTextFillColor: '#FFF',
+                  WebkitBackgroundClip: 'text !important',
+                },
+              },
+              inputLabel: { color: 'white' },
+              textfield_label: { color: 'white' },
+              '& .MuiOutlinedInput-notchedOutline': {
+                border: 'none',
+              },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="password"
+            label="Password"
+            name="password"
+            value={formik.values.password}
+            onFocus={formik.handleBlur}
+            onChange={formik.handleChange}
+            autoComplete="password"
+            InputLabelProps={{ className: 'textfield_label' }}
+            sx={{
+              color: 'white',
+              backgroundColor: 'black',
+              border: !(formik.touched.password && formik.errors.password)
+                ? '1px solid #23BE99'
+                : '1px solid #FF0000',
               borderRadius: '20px',
               outline: 'none',
               input: { color: 'white' },
@@ -219,140 +108,37 @@ function LoginForm() {
               '& .MuiOutlinedInput-notchedOutline': {
                 border: 'none',
               },
-              '@media (max-width: 700px)' : {
-                width: "100%",
-              }
             }}
-          >
-            Registrera
-          </Button>
-        </Box>
-      </Box>
-    </Container>
-  )}else return(
-    <Container component="main" maxWidth="xs">
-      <Box
+          />
+        </Grid>
+      </Grid>
+      <Button
+        onClick={() => loginUser()}
+        type="submit"
+        fullWidth
+        variant="contained"
+        className="center"
+        disabled={!formik.isValid}
         sx={{
-          position: 'absolute',
-          left: "40vw",
-          top: "-20vh",
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          backgroundColor: "black",
-          borderRadius: "1rem",
-          padding: "1rem",
-          '@media (max-width: 700px)' : {
-            position: "relative",
-            left: 0,
-            right: 0,
-          }
+          mt: 3,
+          mb: 2,
+          width: '250px',
+          color: 'white',
+          backgroundColor: '#23BE99',
+          border: '1px solid #23BE99',
+          borderRadius: '20px',
+          outline: 'none',
+          '&:disabled': {
+            color: '#23BE99',
+          },
+          '@media (max-width: 700px)': {
+            width: '100%',
+          },
         }}
       >
-        <Typography
-          component="h1"
-          variant="h5"
-          className="contactMember"
-          sx={{ color: 'white' }}
-        >
-          Logga in
-        </Typography>
-        <Typography
-          component="p"
-          variant="p"
-          className='contactMember'
-          onClick={() => changeLogin()}
-          sx={{ color: "white", textDecoration: "underline", mt: "-1.5rem", ":hover": {
-            color: "#23BE89", cursor: "pointer",
-          } }}>
-            Har du inget konto?
-        </Typography>
-        <Box component="form" onSubmit={handleSend}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                value={email}
-                autoComplete="email"
-                inputProps={{maxLength: 20}}
-                onChange={(event) => setEmail(event.target.value)}
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                autoComplete="password"
-                InputLabelProps={{ className: 'textfield_label' }}
-                sx={{
-                  color: 'white',
-                  backgroundColor: 'black',
-                  border: '1px solid #23BE99',
-                  borderRadius: '20px',
-                  outline: 'none',
-                  input: { color: 'white' },
-                  inputLabel: { color: 'white' },
-                  textfield_label: { color: 'white' },
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: 'none',
-                  },
-                }}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="center"
-            sx={{
-              mt: 3,
-              mb: 2,
-              width: '250px',
-              color: 'white',
-              backgroundColor: '#23BE99',
-              border: '1px solid #23BE99',
-              borderRadius: '20px',
-              outline: 'none',
-              input: { color: 'white' },
-              inputLabel: { color: 'white' },
-              textfield_label: { color: 'white' },
-              '& .MuiOutlinedInput-notchedOutline': {
-                border: 'none',
-              },
-              '@media (max-width: 700px)' : {
-                width: "100%",
-              }
-            }}
-          >
-            Logga in
-          </Button>
-        </Box>
-      </Box>
-    </Container>
+        Logga in
+      </Button>
+    </Box>
   );
 }
 
